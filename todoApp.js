93f8c7479e3e708eaca72a8647dc2bb023c07fe8
wikiwikitang <ctang13@binghamton.edu>
2022-01-09 01:11:13 -0800

    init problem

---
+++ todoApp.js
@@ -0,0 +1,8 @@
+import { todoApi } from './todoApis.js';
+
+/*
+Given an Api, implement a todo app(features includes Add a todo, Change a todo Status, Delete Todo) in Vanilla JavaScript. 
+FYI: You can add some css styles you want.
+*/
+
+//implement your app here
